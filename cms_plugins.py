03f73fe4403ced2c4835be24116583f5b714fca2
Alexander Jung-Loddenkemper <alexander.julo@googlemail.com>
2013-04-05 12:08:45 +0200

    add a smaller version of the news overview

--- cms_plugins.py
+++ cms_plugins.py
@@ -3,7 +3,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
-from .models import NewsFilter
+from .models import NewsFilter, SmallNewsFilter
 
 
 class NewsFilterPlugin(CMSPluginBase):
@@ -14,8 +14,10 @@ class NewsFilterPlugin(CMSPluginBase):
     render_template = "news_overview.html"
 
     def render(self, context, instance, placeholder):
-        pages = context['current_page'].children.all()
-        lang = context['current_language']
+        pages = Page.objects.all()
+        lang = context.get('current_language', None)
+        if not lang:
+            return context
         news = []
         for page in pages:
             # make sure that we only load as many items as we are supposed to.
@@ -32,4 +34,34 @@ class NewsFilterPlugin(CMSPluginBase):
         context['news'] = news
         return context
 
+
+class SmallNewsFilterPlugin(CMSPluginBase):
+    """Renders an overview of all children of this page that use the
+    news.html template (small version)."""
+    model = SmallNewsFilter
+    name = _("Small News Filter")
+    render_template = "news_overview_small.html"
+
+    def render(self, context, instance, placeholder):
+        pages = Page.objects.all()
+        lang = context.get('current_language', None)
+        if not lang:
+            return context
+        news = []
+        for page in pages:
+            # make sure that we only load as many items as we are supposed to.
+            if len(news) == instance.number:
+                break
+            # only load items that are published, available in the right
+            # language and using the right template.
+            if page.template == 'news.html' and lang in page.get_languages() \
+                    and page.published:
+                news.append(page)
+        # sort the object, showing the newest first
+        news = sorted(news, key=lambda x: x.publication_date, reverse=True)
+        # add our news to the context so that we can render it
+        context['smallnews'] = news
+        return context
+
 plugin_pool.register_plugin(NewsFilterPlugin)
+plugin_pool.register_plugin(SmallNewsFilterPlugin)
--- models.py
+++ models.py
@@ -6,3 +6,9 @@ class NewsFilter(CMSPlugin):
     """Saves the settings for the News."""
     # the number of articles that should be displayed.
     number = models.IntegerField(default=5)
+
+
+class SmallNewsFilter(CMSPlugin):
+    """Saves the settings for the News."""
+    # the number of articles that should be displayed.
+    number = models.IntegerField(default=5)
